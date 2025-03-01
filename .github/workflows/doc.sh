5f6991ab3600c14fc5c1eb982ac8aa029db000d9
KillTheMule <KillTheMule@users.noreply.github.com>
2025-03-01 11:54:13 +0100

    Remove doc workflow

--- .github/workflows/doc.sh
+++
@@ -1,5 +0,0 @@
-mkdir doc
-RUSTDOCFLAGS="--cfg docsrs" cargo doc --features use_tokio --no-deps
-mv target/doc/ doc/use_tokio
-RUSTDOCFLAGS="--cfg docsrs" cargo doc --features use_async-std --no-deps
-mv target/doc/ doc/use_async-std
